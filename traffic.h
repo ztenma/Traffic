78f8a7fec93d7b2dfea3c8fc77784e21827c14e1
Tenma <tenma@nuolezio.org>
2016-11-06 18:29:19 +0100

    Added vehicle & trafficController implementations

--- traffic.h
+++ traffic.h
@@ -1,9 +1,18 @@
 # if !defined(TRAFFIC_H)
 # define TRAFFIC_H
 
+# define MAX_VEHICLE_COUNT 16
+
+typedef struct
+{
+    char x; 
+    char y;
+} Pos;
+
 typedef struct
 {
     enum ObjectId objectId : 8; // Code mapped to a text output
+    Pos pos;
     // destination (2), origin (2), parked, align, speed, active
     unsigned char dest   : 2; // N, E, S, W
     unsigned char origin : 2; // N, E, S, W
@@ -13,5 +22,22 @@ typedef struct
     unsigned char parked : 1; 
 } __attribute__((packed)) Vehicle;
 typedef Vehicle *PVehicle;
+
+
+typedef struct
+{
+    Pos no; 
+    Pos se; 
+} Area;
+
+typedef struct
+{
+    PMap map;
+    PVehicle * vehicles;
+    unsigned char vehicleCount;
+    Area areas[];
+    
+} TrafficController;
+typedef TrafficController * PTrafficController;
 
 # endif
